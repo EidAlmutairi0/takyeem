5a38092d9cbbcba733e0baffe8090e22bc93fa41
Eii910 <62373448+Eii910@users.noreply.github.com>
2020-08-14 17:13:32 +0300

    with splash screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -99,6 +99,7 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       child: SafeArea(
+        bottom: false,
         child: Scaffold(
           floatingActionButton: AddDoctorFlotingActionButton(),
           floatingActionButtonLocation:
